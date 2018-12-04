a9067f51178051ab3a15e635c38fb0d5aca67fba
Reiel <niphilim11@gmail.com>
2018-12-04 18:08:12 +0300

    home work unit testing

---
+++ index.js
@@ -0,0 +1,27 @@
+// var x, n;
+// function pow(x,n)
+// {
+//     var result=1;
+//     for(var i=0;i<n;i++) {result*=x;}
+//     return result;
+// }
+
+// x=prompt("x?","");
+// n=prompt("n?","");
+// if (n<0)
+// {
+//     alert("степень "+n+" не поддерживается, введите целую степень, большую 0");
+// }
+// else
+// {
+//     alert(pow(x,n));
+// }
+function calculatePow(x, n) {
+    var result=1;
+    for(var i=0;i<n;i++) {result*=x;}
+    return result;
+}
+
+module.exports = {
+    calculatePow
+};
